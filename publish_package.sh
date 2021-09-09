2095e0176b77324937603409a483a56843acb7dc
Aseem <aseem.hegshetye@gmail.com>
2021-09-09 01:01:10 -0500

    publish_package.sh

--- publish_package.sh
+++ publish_package.sh
@@ -1,4 +1,15 @@
-pip install twine pathlib wheel
+pip install twine pathlib wheel bumpversion
+
+# use current version below
+bumpversion --current-version 3.0.0 major setup.py signpdf2/__init__.py
+
+# empty build folder
+rm -rf ./build
+
+# create build package to deploy
 python setup.py sdist bdist_wheel
+
+# deploy
 twine upload dist/*
-pip uninstall twine pathlib wheel -y
+
+pip uninstall twine pathlib wheel bumpversion -y
